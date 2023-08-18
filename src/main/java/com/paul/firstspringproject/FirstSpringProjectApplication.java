784fa3691d6865da32ff93595c42c1d991481855
PaulDavidTucker <tuckerpauldavid@gmail.com>
2023-08-18 17:49:17 +0100

    Project setup

---
+++ src/main/java/com/paul/firstspringproject/FirstSpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.paul.firstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        SpringApplication.run(FirstSpringProjectApplication.class, args);
+    }
+
+}
